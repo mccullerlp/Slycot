3805571f6f56e0b45750316273691003a6ef4138
Lee McCuller <Lee.McCuller@gmail.com>
2023-12-19 16:43:24 -0600

    made multid

--- slycot/src/CONVF.f
+++ slycot/src/CONVF.f
@@ -1,13 +1,15 @@
       SUBROUTINE CONVF( LDA, A, E, F )
       
-      DOUBLE PRECISION   A(LDA)
-      REAL*16            E(LDA)
-      REAL*10            F(LDA)
+      DOUBLE PRECISION   A(LDA, LDA)
+      REAL*16            E(LDA, LDA)
+      REAL*10            F(LDA, LDA)
       
-      DO 10 I = 1, LDA
-          E(I)=A(I)
-          F(I)=A(I)
-10    CONTINUE
+      DO 20 I = 1, LDA
+         DO 10 J = 1, LDA
+          E(I, J)=A(I, J)
+          F(I, J)=A(I, J)
+10       CONTINUE
+20    CONTINUE
 C
       RETURN
       END
--- slycot/src/CONVFF.f
+++ slycot/src/CONVFF.f
@@ -1,13 +1,15 @@
       SUBROUTINE CONVFF( LDA, A, E, F )
       
-      DOUBLE PRECISION   A(LDA)
-      REAL*16            E(LDA)
-      REAL*10            F(LDA)
+      DOUBLE PRECISION   A(LDA, *)
+      REAL*16            E(LDA, *)
+      REAL*10            F(LDA, *)
       
-      DO 10 I = 1, LDA
-          A(I)=F(I)
-          E(I)=F(I)
-10    CONTINUE
+      DO 20 I = 1, LDA
+         DO 10 J = 1, LDA
+            A(I, J)=F(I, J)
+            E(I, J)=F(I, J)
+ 10      CONTINUE
+ 20   CONTINUE
 C
       RETURN
       END
